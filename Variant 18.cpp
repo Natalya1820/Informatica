8dc21951aaacbce80ba0db401a2d5457d3d4949c
Наталья Курило <kurilonatali2000@mail.ru>
2021-01-16 09:59:00 +0300

    Variant

---
+++ Variant 18.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream> // работа с файлами на компе
+
+using namespace std;
+
+// Напишите функцию, которая по переданному в него имени файла, 
+// открывает его и выводит содержимое на экран, 
+// в случае отсутствия файла выводит сообщение об ошибке.
+
+int main() {
+    cout << "enter file name " << "\n" << endl;
+    string path;
+    cin >> path;
+
+    ifstream fin; // ifstream чтение, а ofstream запись
+    fin.open(path); // файл открывается в директории проекта
+
+    if (!fin.is_open()) // проверка удалось ли открыть
+    {
+        cout << "file not open" << endl;
+    }
+    else
+    {
+        cout << "ok, successful" << "\n" << endl;
+        cout << "file name: " << path << "\n" << endl;
+        char ch;
+        while (fin.get(ch)) //считывает символ из файла и записывает в переменную (по 1 симоволу)
+            // метод while потому что get даёт либо true, либо false.
+
+        {
+            cout << ch;
+        }
+
+    }
+    fin.close(); //освободить ресурсы
+
+    return 0;
+
+
+}
